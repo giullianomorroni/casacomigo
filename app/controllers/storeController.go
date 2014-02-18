de39d1c6fd38d49bb3c9f078f72f161091acdba7
giulliano morroni <giulliano@localhost.localdomain>
2014-02-18 14:08:15 -0300

    correcoes loja

--- app/controllers/storeController.go
+++ app/controllers/storeController.go
@@ -26,3 +26,15 @@ func (this *Store) Store(apelido string) revel.Result {
 func (this *Store) Products() revel.Result {
 	return this.Render();
 }
+
+func (this *Store) Checkout(product_id int) revel.Result {
+	return this.Render();
+}
+
+func (this *Store) Product(product_id int) revel.Result {
+	return this.Render();
+}
+
+func (this *Store) Faq() revel.Result {
+	return this.Render();
+}
